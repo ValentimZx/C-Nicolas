8ddc34eb73a0a7bf672b9b695f284ae27e07873f
Nicolas <nvalentim824@gmail.com>
2025-06-20 20:42:07 -0300

    Add Codigos

--- Precedencia/Program.cs
+++ Precedencia/Program.cs
@@ -1,8 +1,20 @@
 ﻿Console.WriteLine("## Operadores de Precedencia ##\n");
 
-int x = 10 - 2 * 3;
+//int x = 10 - 2 * 3;
 
-int y = (10 - 2) * 3;
+//int y = (10 - 2) * 3;
 
-Console.WriteLine($"Valor de y e: {y}");
-Console.WriteLine($"Valor de x e: {x}");
+//Console.WriteLine($"Valor de y e: {y}");
+//Console.WriteLine($"Valor de x e: {x}");
+
+//bool b = !(9 != 8) && (5 >= 7 || 8 >= 6);
+//Console.WriteLine(b);
+
+//int a = 5, c = 6, d = 4;
+//int r = ((--a) * c) - (++c);
+
+//Console.WriteLine(r);
+
+int a = 5, b = 6, c = 4;
+int r = --a * b - ++c;
+Console.WriteLine(r);
