f15f481b0972480da2d3cb76d5d57037cf226997
Nicolas <nvalentim824@gmail.com>
2025-06-21 17:35:05 -0300

    Commit add op u, t

---
+++ Nullable_Types_Reference/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("## Tipos de referencias Nullable Types/n");
+
+//string nome = null;
+//Console.WriteLine(nome.ToUpper());
+
+string ? nome = null;
+
+Console.WriteLine(nome?.ToUpper()); //Se a variavel nome for null, ele vai atribuir valor null a variavel "nome", se nome nao for null, ele vai atribuir valor a variavel "nome".
+
+
+
+
+Console.ReadKey();
---
+++ Operador_Unitario_Ternario/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("## Operador Uniario e Ternario/n");
+
+//Uniario (+) Uniario (-) Ternario (?:)
+
+int positivo = 1;
+int resultado;
+
+resultado = +positivo;
+Console.WriteLine(resultado);
+
+
+
+Console.ReadKey();
